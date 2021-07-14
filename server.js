feef02113bab2e43a2c60b5a05595748499b575a
Tuan Nguyen <tuan.nguyen@essensys.tech>
2021-07-14 14:52:19 +0100

    update

--- server.js
+++ server.js
@@ -47,8 +47,7 @@ function broadcastEvents(newEvent) {
     clients.forEach(client => client.response.write(`data: ${JSON.stringify(newEvent)}`))
 }
 async function notifyEvent(request, response, next) {
-    response.set('Content-Type', 'text/plain');
-    response.send('accepted');
+    response.json({challenge : 'verified'});
     return broadcastEvents(request.body);
 }
 app.post('/events', notifyEvent);
